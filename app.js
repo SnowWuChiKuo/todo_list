d8564de5f24c712b7c8894a2c21b15893bc4b550
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-04-17 01:38:26 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
+const flash = require('connect-flash/lib/flash')
 require('./config/mongoose')
 
 const app = express()
@@ -25,10 +26,13 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
-  console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
