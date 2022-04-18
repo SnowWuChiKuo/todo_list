be19a8f372cfd0c200c7b19f55ab484f15ecb2ab
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-04-19 00:58:02 +0800

    update mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = "mongodb+srv://root:texttiger891@cluster0.rntyw.mongodb.net/todo-list?retryWrites=true&w=majority"
+const MONGODB_URI = "mongodb+srv://root:texttiger891@cluster0.rntyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
